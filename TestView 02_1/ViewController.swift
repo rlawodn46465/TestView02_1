3ea6bada33192af9d15b39629a6625d22f5aca33
dit08 <dit@imac-3.local>
2019-05-02 16:46:37 +0900

    과제

--- TestView 02_1/ViewController.swift
+++ TestView 02_1/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        myTableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
     }
 
+    public func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0 {
+            return "Section Header 1"
+        } else {
+            return "Section Header 2"
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == 0{
+            return "Section Footer 1"
+        } else {
+            return "Section Footer 2"
+        }
+    }
 
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //cell 생성, 재활용 풀 사용
+        let cell = myTableView.dequeueReusableCell(withIdentifier: "RE", for: indexPath)
+        let row = indexPath.row
+        print("section = \(indexPath.section) row : \(indexPath.row)")
+        
+        cell.textLabel?.text = "section : \(indexPath.section)"
+        cell.detailTextLabel?.text = "row : \(indexPath.row)"
+        
+        return cell
+    }
 }
 
